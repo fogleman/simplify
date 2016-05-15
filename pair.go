77245f3f0c94a8451dd084d4d6afa19188a52693
Michael Fogleman <fogleman@gmail.com>
2016-05-14 21:20:10 -0400

    performance improvement

--- pair.go
+++ pair.go
@@ -12,16 +12,17 @@ func MakePairKey(a, b *Vertex) PairKey {
 }
 
 type Pair struct {
-	A, B    *Vertex
-	Index   int
-	Removed bool
+	A, B        *Vertex
+	Index       int
+	Removed     bool
+	CachedError float64
 }
 
 func NewPair(a, b *Vertex) *Pair {
 	if b.Less(a.Vector) {
 		a, b = b, a
 	}
-	return &Pair{a, b, -1, false}
+	return &Pair{a, b, -1, false, -1}
 }
 
 func (p *Pair) Quadric() Matrix {
@@ -33,6 +34,9 @@ func (p *Pair) Vector() Vector {
 }
 
 func (p *Pair) Error() float64 {
-	q := p.Quadric()
-	return q.QuadricError(q.QuadricVector())
+	if p.CachedError < 0 {
+		q := p.Quadric()
+		p.CachedError = q.QuadricError(q.QuadricVector())
+	}
+	return p.CachedError
 }
